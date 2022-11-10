c8944e0e7ed6b8925ea0526993bcfb889e9dfdc2
Pavel Krimont <kripalser@ya.ru>
2022-11-10 22:50:29 +0600

    Adjust `nextUntil` and `nextAll` functions

--- utils.js
+++ utils.js
@@ -1,28 +1,26 @@
 // https://vanillajstoolkit.com/helpers/nextuntil/
-/*!
+/*
  * Get all following siblings of each element up to but not including the element matched by the selector
  * (c) 2017 Chris Ferdinandi, MIT License, https://gomakethings.com
  * @param  {Node}    elem                   The element
  * @param  {String}  selector               The selector to stop at
- * @param  {Boolean} [includeFirst=false]   Whether to get also the first element
+ * @param  {Boolean} [includeElem=false]    Whether to include the element itself
  * @param  {String}  filter                 The selector to match siblings against [optional]
  * @return {Array}                          The siblings
  */
-const nextUntil = (elem, selector, includeFirst = false, filter) => {
-
+const nextUntil = (elem, selector, includeElem = false, filter) => {
     // Setup siblings array
-    var siblings = [];
+    const siblings = [];
 
     if (elem === undefined) return siblings;
 
-    if (includeFirst === false) {
+    if (includeElem === false) {
         // Get the next sibling element
         elem = elem.nextElementSibling;
     }
 
     // As long as a sibling exists
     while (elem) {
-
         // If we've reached our match, bail
         if (elem.matches(selector)) break;
 
@@ -37,7 +35,6 @@ const nextUntil = (elem, selector, includeFirst = false, filter) => {
 
         // Get the next sibling element
         elem = elem.nextElementSibling;
-
     }
 
     return siblings;
@@ -45,18 +42,24 @@ const nextUntil = (elem, selector, includeFirst = false, filter) => {
 };
 
 // https://gist.github.com/firestar300/317d8f0cb7b6764346fe02c9223db9d1
+/*
+ * Get all following siblings of element
+ * @param {Node} elem   The element
+ * @return {Array}      The siblings
+ */
 const nextAll = (elem) => {
-    const nextElements = [];
-    let nextElement = elem;
+    const siblings = [];
 
-    if (elem === undefined) return nextElements;
+    if (elem === undefined) return siblings;
 
-    while (nextElement.nextElementSibling) {
-        nextElements.push(nextElement.nextElementSibling);
-        nextElement = nextElement.nextElementSibling;
+    elem = elem.nextElementSibling;
+
+    while (elem) {
+        siblings.push(elem);
+        elem = elem.nextElementSibling;
     }
 
-    return nextElements;
+    return siblings;
 };
 
 module.exports = { nextUntil, nextAll };
